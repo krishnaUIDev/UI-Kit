15fc244288e96a031659e441ff40135571df5eec
krishnaUIDev <krishna.kondoju007@gmail.com>
2020-08-05 20:18:09 -0400

    alen added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
@@ -11,12 +11,27 @@ import Download from '@theme/Download';
 import Blog from '@theme/Blog';
 import OptinChat from '@theme/OptinChat';
 import styles from './styles.module.scss';
+import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment';
 
+const alanKey =
+  '0dce19ac79b6af76620cc281f4d8ab092e956eca572e1d8b807a3e2338fdd0dc/stage';
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
   const {tagline} = siteConfig;
-
+  useEffect(() => {
+    if (ExecutionEnvironment.canUseDOM) {
+      const alanBtn = require('@alan-ai/alan-sdk-web');
+      alanBtn({
+        key: alanKey,
+        onCommand: ({command, items}) => {
+          if (command === 'newTechNews') {
+            console.log(items, 'items');
+          }
+        },
+      });
+    }
+  }, []);
   return (
     <Layout description={tagline}>
       <Hero />
